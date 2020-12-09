e9ab5fff1d608624818d5a0afef0f4e892a46314
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-12-08 21:16:15 -0400

    Add webp image format

--- src/components/Juice/index.tsx
+++ src/components/Juice/index.tsx
@@ -3,11 +3,17 @@ import React, { useEffect, useState } from 'react';
 import Parallax from 'parallax-js';
 
 import juice from '../../assets/img/juice.png';
+import juiceWeb from '../../assets/webp/juice.webp';
 import leafOne from '../../assets/img/leaf01.png';
+import leafOneWeb from '../../assets/webp/leaf01.webp';
 import leafTwo from '../../assets/img/leaf02.png';
+import leafTwoWeb from '../../assets/webp/leaf02.webp';
 import leafThree from '../../assets/img/leaf03.png';
+import leafThreeWeb from '../../assets/webp/leaf03.webp';
 import leafFour from '../../assets/img/leaf04.png';
+import leafFourWeb from '../../assets/webp/leaf04.webp';
 import leafFive from '../../assets/img/leaf05.png';
+import leafFiveWeb from '../../assets/webp/leaf05.webp';
 
 import './styles.css';
 
@@ -29,25 +35,49 @@ const Juice: React.FC = () => {
   return (
     <>
       <div className="juice">
-        <img src={juice} alt="Juice" />
+        <picture>
+          <source srcSet={juiceWeb} type="image/webp" />
+          <source srcSet={juice} type="image/png" />
+          <img src={juice} alt="Juice" />
+        </picture>
       </div>
 
       <div className="leaves">
         <ul id="scene">
           <li className="layer" data-depth="-.1">
-            <img src={leafOne} alt="" />
+            <picture>
+              <source srcSet={leafOneWeb} type="image/webp" />
+              <source srcSet={leafOne} type="image/png" />
+              <img src={leafOne} alt="Juice" />
+            </picture>
           </li>
           <li className="layer" data-depth="-.3">
-            <img src={leafTwo} alt="" />
+            <picture>
+              <source srcSet={leafTwoWeb} type="image/webp" />
+              <source srcSet={leafTwo} type="image/png" />
+              <img src={leafTwo} alt="Juice" />
+            </picture>
           </li>
           <li className="layer" data-depth="-1.5">
-            <img src={leafThree} alt="" />
+            <picture>
+              <source srcSet={leafThreeWeb} type="image/webp" />
+              <source srcSet={leafThree} type="image/png" />
+              <img src={leafThree} alt="Juice" />
+            </picture>
           </li>
           <li className="layer" data-depth=".1">
-            <img src={leafFour} alt="" />
+            <picture>
+              <source srcSet={leafFourWeb} type="image/webp" />
+              <source srcSet={leafFour} type="image/png" />
+              <img src={leafFour} alt="Juice" />
+            </picture>
           </li>
           <li className="layer" data-depth=".3">
-            <img src={leafFive} alt="" />
+            <picture>
+              <source srcSet={leafFiveWeb} type="image/webp" />
+              <source srcSet={leafFive} type="image/png" />
+              <img src={leafFive} alt="Juice" />
+            </picture>
           </li>
         </ul>
       </div>
