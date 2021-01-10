69e6ac36df42b71e144574faafb2dbd5703d882e
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-01-10 13:38:53 -0400

    Use regular functions to declare components

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ import Routes from './routes';
 import GlobalStyle from './styles/global';
 import theme from './styles/theme';
 
-const App: React.FC = () => {
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Routes />
     </ThemeProvider>
   );
-};
+}
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from '../pages/Home';
 
-const Routes: React.FC = () => {
+function Routes() {
   return (
     <BrowserRouter>
       <Switch>
@@ -12,6 +12,6 @@ const Routes: React.FC = () => {
       </Switch>
     </BrowserRouter>
   );
-};
+}
 
 export default Routes;
